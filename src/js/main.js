436e133a943e5ff6455584347b422e129b5ec6df
dheflo <florentin.dhenau@gmail.com>
2026-05-21 14:49:17 +0200

    Added a plane, sphere, lights, shadow and anv

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,48 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-//scene and camera setup
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Renderer setup
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 5, 2);
+camera.lookAt(0, 0, 0);
 
-scene.add(cube);
+const scene = new THREE.Scene();
 
-// Position the camera
-camera.position.z = 5;
+/* ORBIT CONTROL*/
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// Animate the cube
-function animate(time) {
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
-  renderer.render(scene, camera);
+/* SPHERE */
+const geometry = new THREE.IcosahedronGeometry(1,4);
+const material = new THREE.MeshStandardMaterial({color: 0x0000ff, flatShading: true});
+const sphere = new THREE.Mesh(geometry,material);
+sphere.castShadow = true;
+scene.add(sphere);
+
+sphere.position.y = 3;
+
+/* GROUND */
+const groundGeo = new THREE.PlaneGeometry(10,10);
+const groundMat = new THREE.MeshStandardMaterial({color: 0x222222, side: THREE.DoubleSide});
+const ground = new THREE.Mesh(groundGeo, groundMat)
+ground.receiveShadow = true;
+scene.add(ground);
+
+ground.rotation.x = Math.PI/2;
+
+const directionalLight = new THREE.DirectionalLight(0xff0000, 100);
+directionalLight.castShadow = true;
+directionalLight.position.set(0, 10, 0);
+scene.add(directionalLight);
+
+function animate(t = 0){
+  requestAnimationFrame(animate);
+  sphere.rotation.y = t * 0.0001;
+  sphere.rotation.x = t * 0.00005;
+  renderer.render(scene,camera);
 }
 
-
-
-// Responsive window
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+animate();
